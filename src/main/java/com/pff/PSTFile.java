1bca1a28361ef2ace4ee993295696006f45503c1
Ed Pfromer <epfromer@gmail.com>
2018-02-20 17:48:32 -0700

    get UUID parsing working

--- src/main/java/com/pff/PSTFile.java
+++ src/main/java/com/pff/PSTFile.java
@@ -251,11 +251,7 @@ public class PSTFile {
             } else {
                 uuidIndexes[i] = -1; // We don't know this guid
             }
-            /*
-             * System.out.printf("uuidArray[%d] = {%s},%d\n", i,
-             * uuidArray[i].toString(), uuidIndexes[i]);
-             * /
-             **/
+            System.out.printf("uuidArray[%d] = {%s},%d\n", i, uuidArray[i].toString(), uuidIndexes[i]);
             offset += 16;
         }
 
